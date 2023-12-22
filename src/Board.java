7767f6856b695c6dc3929fcd007e559768ffc5cd
technopyrax <138320622+technopyrax@users.noreply.github.com>
2023-12-22 09:27:23 -0600

    update

---
+++ src/Board.java
@@ -0,0 +1,9 @@
+public class Board {
+
+    private ArrayList<Space> board;
+
+    public Board(){
+        board = new ArrayList<>();
+    }
+
+}
---
+++ src/Space.java
@@ -0,0 +1,12 @@
+public class Space{
+
+    private char state;
+
+    public void getPos(){
+        return this.state;
+    }
+
+    public void setPos(char state){
+        this.state = state;
+    }
+}
